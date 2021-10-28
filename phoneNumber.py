79b9341d9fe7b5f9389ec2a628ff516ad7ac3ee8
Rajveer <93126793+Rajveer2009@users.noreply.github.com>
2021-10-28 20:12:38 +0530

    Update phoneNumber.py

--- phoneNumber.py
+++ phoneNumber.py
@@ -1,14 +1,14 @@
-#improting libs
+#improting libraries 
 import phonenumbers
 from phonenumbers import carrier, geocoder
 
-#defining vars
+#defining variables
 number = "+91" + input("What phone number do you wanna\nknow info about? ")
 phoneNumber = phonenumbers.parse(number)
 Carrier = carrier.name_for_number(phoneNumber, 'en')
 Region = geocoder.description_for_number(phoneNumber, 'en')
 
-#printing them
+#printing variables
 print(phoneNumber)
 print(Carrier)
 print(Region)
