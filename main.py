d358992f04e9fb9219ff112872e452b6364313ab
Rajveer <rajveerg2009@gmail.com>
2022-06-09 18:46:05 +0530

    Changes to be committed: 	new file:   main.py
    Changes not staged for commit:
    	deleted:    phoneNumber.py

---
+++ main.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+
+#improting libraries 
+import phonenumbers
+from phonenumbers import carrier, geocoder
+
+#defining variables
+number = "+91" + input("What phone number do you wanna\nknow info about? ")
+phoneNumber = phonenumbers.parse(number)
+Carrier = carrier.name_for_number(phoneNumber, 'en')
+Region = geocoder.description_for_number(phoneNumber, 'en')
+
+#printing variables
+print(phoneNumber)
+print(Carrier)
+print(Region)
