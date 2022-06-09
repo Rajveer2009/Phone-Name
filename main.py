f6dc9e5a92f8ab098d4f24118a3c27d34a9657cb
Rajveer <rajveerg2009@gmail.com>
2022-06-09 19:12:55 +0530

    Changes to be committed: 	modified:   main.py
    Changes not staged for commit:
    	deleted:    phoneNumber.py
    
     Untracked files:
    	.gitignore

--- main.py
+++ main.py
@@ -4,13 +4,29 @@
 import phonenumbers
 from phonenumbers import carrier, geocoder
 
-#defining variables
-number = "+91" + input("What phone number do you wanna\nknow info about? ")
-phoneNumber = phonenumbers.parse(number)
-Carrier = carrier.name_for_number(phoneNumber, 'en')
-Region = geocoder.description_for_number(phoneNumber, 'en')
+#main
 
-#printing variables
-print(phoneNumber)
-print(Carrier)
-print(Region)
+n = 1
+
+def banner():
+        print  ("""\033[94m
+████████╗██████╗  █████╗  ██████╗██╗  ██╗███████╗██████╗
+╚══██╔══╝██╔══██╗██╔══██╗██╔════╝██║ ██╔╝██╔════╝██╔══██╗
+   ██║   ██████╔╝███████║██║     █████╔╝ █████╗  ██████╔╝
+   ██║   ██╔══██╗██╔══██║██║     ██╔═██╗ ██╔══╝  ██╔══██╗
+   ██║   ██║  ██║██║  ██║╚██████╗██║  ██╗███████╗██║  ██║
+   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
+        \033""")
+
+def do():
+    banner()
+    print("CTRL + z for exit")
+    number = "+91" + input("What phone number do you wanna\nknow info about? ")
+    phoneNumber = phonenumbers.parse(number)
+    Carrier = carrier.name_for_number(phoneNumber, 'en')
+    Region = geocoder.description_for_number(phoneNumber, 'en')
+    print(phoneNumber)
+    print(Carrier)
+    print(Region)
+
+do() 
